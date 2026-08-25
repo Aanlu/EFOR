f09cf86da719f4a9bb15da5180be9b2e5eeebd38
Luis Antonio <luisantonio6641@gmail.com>
2026-08-24 19:34:50 -0700

    test(logging): add unit tests for file logger verification

--- src/core/logger.c
+++ src/core/logger.c
@@ -1,4 +1,7 @@
 #include "core/logger.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
 
 static const char* log_level_to_string(LogLevel level) {
     switch (level) {
@@ -9,4 +12,31 @@ static const char* log_level_to_string(LogLevel level) {
         case LOG_LEVEL_FATAL: return "FATAL";
         default:              return "UNKNOWN";
     }
+}
+
+void logger_log(LogLevel level, const char* file, int line, const char* format, ...) {
+    FILE* log_file = fopen("debug.log", "a");
+    if (!log_file) {
+        return;
+    }
+
+    time_t raw_time = time(NULL);
+    struct tm* time_info = localtime(&raw_time);
+
+    char time_str[32];
+    if (time_info) {
+        strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", time_info);
+    } else {
+        snprintf(time_str, sizeof(time_str), "0000-00-00 00:00:00");
+    }
+
+    fprintf(log_file, "[%s] [%s] [%s:%d] ", time_str, log_level_to_string(level), file, line);
+
+    va_list args;
+    va_start(args, format);
+    vfprintf(log_file, format, args);
+    va_end(args);
+
+    fprintf(log_file, "\n");
+    fclose(log_file);
 }
